211abbb299f40f39f8f6753316946a4dfab5c4ef
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-05-15 22:41:00 +0200

    fix(button): update styles to match design

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,14 +10,16 @@ const Button = ({ children, type, onClick, className = '' }: ButtonProps) => {
 	return (
 		<button
 			type='button'
-			className={`h-12 px-6 rounded-2xl font-bold transition ease-out duration-500 hover:border-neutral-300 hover:bg-neutral-100 hover:text-neutral-300
+			className={`lg:text-[16px] text-[14px] h-12 px-6 rounded-2xl font-bold transition ease-out duration-500 hover:border-neutral-300 hover:bg-neutral-100 hover:text-neutral-300
         ${
 					type === 'filled'
 						? 'bg-neutral-300 text-neutral-100 hover:border'
 						: ''
 				}
         ${
-					type === 'outline' ? 'border border-neutral-200 text-neutral-200' : ''
+					type === 'outline'
+						? 'border-2 border-neutral-200 text-neutral-200'
+						: ''
 				}
         ${className}
       `}
