722725551f26d95bcb017abc36bf6d733ddabac2
likelytwitchdollop <cynthiamahofa@yahoo.com>
2023-06-26 15:18:12 +0200

    feat: create home page

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import Accordion from './Accordion'
+import Button from './Button'
+import Dropdown from './Dropdown'
+import Header from './Header'
+
+export { Accordion, Button, Dropdown, Header }
